b817d74292f0da92a50400c14e5429019157e7eb
Sri Rizki Batu Bara <sririzki.02062000@gmail.com>
2021-10-12 13:23:29 +0700

    java-challenge-1

---
+++ KikiJava.java
@@ -0,0 +1,15 @@
+public class KikiJava{
+    public static void main(String[] args){
+    String Fullname = "SriRizkiBatubara";
+    int numberAge =  21;
+    String address = "Padangsidimpuan";
+    String phoneNumber =  "082736673589";
+    String hobby = "Writing";
+    String favoriteFood = "Sate";
+    System.out.println ("Fullname :" + Fullname );
+        System.out.println ( "numberAge :" + numberAge);
+        System.out.println ( "address :" + address);
+        System.out.println (  "phoneNumber :" + phoneNumber);
+        System.out.println ( "hobby :" + hobby);
+        System.out.println ( "favoriteFood :" + favoriteFood); 
+    }}
